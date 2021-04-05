673f2ea12ea802577f60dcbf9c779a14ce75ca14
Hieu Bui <hieu.bui@merrillcorp.com>
2021-04-05 14:47:13 -0600

    try hashrouter

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -1,7 +1,7 @@
 // Startup point for the client side application
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import axios from 'axios';
@@ -20,8 +20,11 @@ const store = createStore(
 
 export const client = (
   <Provider store={store}>
+    {/* Switch to HashRouter for no server side rendering and only client side rendering */}
     <BrowserRouter>
+    {/*<HashRouter>*/}
       <div>{renderRoutes(Routes)}</div>
+    {/*</HashRouter>*/}
     </BrowserRouter>
   </Provider>
 );
--- src/server/app.ts
+++ src/server/app.ts
@@ -8,6 +8,7 @@ app.set('port', process.env.PORT || 3000);
 app.use('/api', apiRouter);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+//need to handle favicon
 app.get('*', uiRouter);
 
 export default app;
